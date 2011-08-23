ef544c4892f09636c381d44bdeb3f87a9e204ddd
kleung <kleung@redhat.com>
2011-08-23 11:22:59 -0400

    Fixed an issue with rpm string not excluding non-package name properly.

--- org.eclipse.mylyn.koji.core/src/org/eclipse/mylyn/koji/client/internal/utils/KojiTaskParsingUtility.java
+++ org.eclipse.mylyn.koji.core/src/org/eclipse/mylyn/koji/client/internal/utils/KojiTaskParsingUtility.java
@@ -147,8 +147,20 @@ public final class KojiTaskParsingUtility {
 				String fullRpmStr = (String) requestStr;
 				String rpm = fullRpmStr.substring(
 						fullRpmStr.lastIndexOf("/") + 1, fullRpmStr.length()); //$NON-NLS-1$
-				rpm.replaceAll("[^a-zA-Z0-9-]", "|");
-				rpm = rpm.substring(0, rpm.indexOf('|'));
+				rpm = rpm.replaceAll("[^a-zA-Z0-9-]", "|");
+				if(rpm.indexOf('|') > 0)
+					rpm = rpm.substring(0, rpm.indexOf('|'));
+				if(rpm.contains("-")) {
+					String lastChunk = rpm.substring(rpm.lastIndexOf("-") + 1, rpm.length());
+					boolean exceptionThrown = false;
+					try {
+						Integer.parseInt(lastChunk);
+					} catch(NumberFormatException e) {
+						exceptionThrown = true;
+					}
+					if(!exceptionThrown)
+						rpm = rpm.substring(0, rpm.lastIndexOf("-"));
+				}
 				task.setRpm(rpm);
 			}
 			requestStr = requestStrings[1];
